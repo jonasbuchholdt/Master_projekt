57d9c233a2e41e86f674e7e27d3e3f8b508f05c0
jonas buchholdt <jbuchh13@student.aau.dk>
2019-02-26 13:44:09 +0100

    update 26 feb

--- code/crosswind_geo.m
+++ code/crosswind_geo.m
@@ -9,7 +9,7 @@
 clear 
 v = 10;
 wind = pi/2;
-c = 343;
+c_r = 343;
 step = 65;
 speaker_dis = 20;
 %dis = 0.029;
@@ -17,54 +17,56 @@ speaker_dis = 20;
 front = [0; 1];
 point_l = [0; 0];
 %direction(:,1) = [-0.9; 0.1]
-direction(:,1) = [-0.707; 0.707]
-direction(:,1) = 1/norm(direction(:,1),2)*direction(:,1)
-leng_l(1) = norm(direction(:,1),2)
+a_direction(:,1) = [-0.707; 0.707]
+a_direction(:,1) = 1/norm(a_direction(:,1),2)*a_direction(:,1)
+leng_l(1) = norm(a_direction(:,1),2)
 
-theta(1) = sign(direction(1,1))*acos((direction(:,1)'*[0 1]')/(norm(direction(:,1))*1)); %asin((dis)/(1));
+theta(1) = sign(a_direction(1,1))*acos((a_direction(:,1)'*[0 1]')/(norm(a_direction(:,1))*1)); %asin((dis)/(1));
 for i=1:step  
-    d(:,i) = [v*(1/(c+v*sin(theta(i))))*sin(wind); v*(1/(c+v*sin(theta(i))))*cos(wind)]; 
-    t_l(:,i) = (1/(c+v*sin(theta(i))))*sin(wind);
-    p(:,i) = (d(:,i)'*(-direction(:,i)))/norm(direction(:,i))^2*(-direction(:,i));
-    e(:,i) = d(:,i)-p(:,i);
-    ang = asin(norm(e(:,i)));
-    le = cos(ang);
-    b = direction(:,i)*le;
-    direction(:,i+1) = b+e(:,i);
-    leng_l(i+1) = norm(direction(:,i+1),2);
-    theta(i+1) = sign(direction(1,i+1))*acos((direction(:,i+1)'*front)/(norm(direction(:,i+1))*1));
-    dis(:,i) = ((direction(:,i+1)'*front)/(norm(front)^2))*(front); 
-    point_l(:,i+1) = point_l(:,i)+direction(:,i+1);
-end
+d(:,i) = [v * (1/(c_r+v*sin(theta(i)))) * sin(wind); v * (1/(c_r+v*sin(theta(i)))) * cos(wind)]; 
+p(:,i) = (d(:,i)' * (-a_direction(:,i)))/norm(a_direction(:,i))^2 * (-a_direction(:,i));
+e(:,i) = d(:,i) - p(:,i);
+phi(i) = asin(norm(e(:,i)));
+a(:,i) = a_direction(:,i) * cos(phi(i));
+w(:,i) = a(:,i) + e(:,i);
+theta(i+1) = sign(w(1,i)) * acos(((w(:,i)'*front)) / (norm(w(:,i))*norm(front)));
+a_direction(:,i+1) = w(:,i);
 
+leng_l(i+1) = norm(w(:,i),2);
+point_l(:,i+1) = point_l(:,i)+w(:,i);
+dis_l(:,i) = ((w(:,i)'*front)/(norm(front)^2))*(front); 
+t_l(:,i) = (1/(c_r+v*sin(theta(i))))*sin(wind);
+end
+%%
 point_r = [0; 0];
 %direction(:,1) = [0.9; 0.1]
-direction(:,1) = [0.707; 0.707]
-direction(:,1) = 1/norm(direction(:,1),2)*direction(:,1)
-leng_r(1) = norm(direction(:,1),2)
+a_direction(:,1) = [0.707; 0.707]
+a_direction(:,1) = 1/norm(a_direction(:,1),2)*a_direction(:,1)
+leng_r(1) = norm(a_direction(:,1),2)
 
-theta(1) = sign(direction(1,1))*acos((direction(:,1)'*[0 1]')/(norm(direction(:,1))*1)); %asin((dis)/(1));
+theta(1) = sign(a_direction(1,1))*acos((a_direction(:,1)'*[0 1]')/(norm(a_direction(:,1))*1)); %asin((dis)/(1));
 for i=1:step+step
     
-    d(:,i) = [v*(1/(c+v*sin(theta(i))))*sin(wind); v*(1/(c+v*sin(theta(i))))*cos(wind)]; 
-    t_r(:,i) = (1/(c+v*sin(theta(i))))*sin(wind);
-    p(:,i) = (d(:,i)'*(-direction(:,i)))/norm(direction(:,i))^2*(-direction(:,i));
-    e(:,i) = d(:,i)-p(:,i);
-    ang = asin(norm(e(:,i)));
-    le = cos(ang);
-    b = direction(:,i)*le;
-    direction(:,i+1) = b+e(:,i);   
-    leng_r(i+1) = norm(direction(:,i+1),2);
-    theta(i+1) = sign(direction(1,i+1))*acos((direction(:,i+1)'*front)/(norm(direction(:,i+1))*1));
-    dis(:,i) = ((direction(:,i+1)'*front)/(norm(front)^2))*(front); 
-    point_r(:,i+1) = point_r(:,i)+direction(:,i+1);
+d(:,i) = [v*(1/(c_r+v*sin(theta(i))))*sin(wind); v*(1/(c_r+v*sin(theta(i))))*cos(wind)]; 
+p(:,i) = (d(:,i)'*(-a_direction(:,i)))/norm(a_direction(:,i))^2 * (-a_direction(:,i));
+e(:,i) = d(:,i)-p(:,i);
+phi(i) = asin(norm(e(:,i)));
+a(:,i) = a_direction(:,i)*cos(phi(i));
+w(:,i) = a(:,i)+e(:,i);
+theta(i+1) = sign(w(1,i))*acos(((w(:,i)'*front)) / (norm(w(:,i))*norm(front)));
+a_direction(:,i+1) = w(:,i);
+
+leng_r(i+1) = norm(w(:,i),2);
+point_r(:,i+1) = point_r(:,i)+w(:,i);
+dis_r(:,i) = ((w(:,i)'*front)/(norm(front)^2))*(front); 
+t_r(:,i) = (1/(c_r+v*sin(theta(i))))*sin(wind);
 
     if sum(t_r) >= sum(t_l)
         break
     end
 end
 
-sum(dis(2,:))
+%sum(dis(2,:))
 
 plot([point_l(1,:)+speaker_dis/2 flip(point_r(1,:))+speaker_dis/2],[point_l(2,:) flip(point_r(2,:))],'Color',[0.6 0.6 0.6])
 hold on

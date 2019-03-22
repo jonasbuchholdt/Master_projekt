50b896337893516bd029a86abe08ab097669cd0e
jonas buchholdt <jbuchh13@student.aau.dk>
2019-03-22 21:10:40 +0100

    update 22 marts

--- Acoustics/sine_sweep_v4/wind_tunnel_measurement.m
+++ Acoustics/sine_sweep_v4/wind_tunnel_measurement.m
@@ -115,15 +115,40 @@ data(:,6) = humidity_m;
 
 %%
 fs = 44100;
-load('ingen_kile_vs_ingen.mat')
+
+
+load('ingen_vind.mat')
 [tf1u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
 [tf1m,w] = freqz((data(:,2)/0.131931494139380)/length(data),1,20000,fs);
 
-load('ingen_kile_vs_ingen_2.mat')
+load('ingen_vind_2.mat')
 [tf2u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
 [tf2m,w] = freqz((data(:,2)/0.131931494139380)/length(data),1,20000,fs);
 
-load('ingen_kile_vs_ingen_3.mat')
+load('ingen_vind_3.mat')
+[tf3u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
+[tf3m,w] = freqz((data(:,2)/0.131931494139380)/length(data),1,20000,fs);
+
+
+%out_ref = (tf1m+tf2m+tf3m)/3;
+%db_ref = 20*log10(abs(out_ref/(20*10^-6)));
+%db_mref = [db_ref(1:100); movmean(downsample(db_ref(100+1:1000),2),3); movmean(downsample(db_ref(1000+1:10000),15),2); movmean(downsample(db_ref(10000+1:end),30),2)];
+
+out_ref = (tf1u+tf2u+tf3u)/3;
+db_ref = 20*log10(abs(out_ref/(20*10^-6)));
+db_uref = [db_ref(1:100); movmean(downsample(db_ref(100+1:1000),2),3); movmean(downsample(db_ref(1000+1:10000),15),2); movmean(downsample(db_ref(10000+1:end),30),2)];
+
+
+
+load('stor_kile_vs_ingen.mat')
+[tf1u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
+[tf1m,w] = freqz((data(:,2)/0.131931494139380)/length(data),1,20000,fs);
+
+load('stor_kile_vs_ingen_2.mat')
+[tf2u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
+[tf2m,w] = freqz((data(:,2)/0.131931494139380)/length(data),1,20000,fs);
+
+load('stor_kile_vs_ingen_3.mat')
 [tf3u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
 [tf3m,w] = freqz((data(:,2)/0.131931494139380)/length(data),1,20000,fs);
 
@@ -132,18 +157,187 @@ out_m = (tf1m+tf2m+tf3m)/3;
 
 db_u = 20*log10(abs(out_u/(20*10^-6)));
 db_m = 20*log10(abs(out_m/(20*10^-6)));
-db = db_m;%-db_m;
+%db = db_u-db_m;
 %db = movmean(db,5);
-db = [db(1:100); movmean(downsample(db(100+1:1000),2),3); movmean(downsample(db(1000+1:10000),15),2); movmean(downsample(db(10000+1:end),30),2)];
-w =  [w(1:100);  movmean(downsample(w(100+1:1000),2),3); movmean(downsample(w(1000+1:10000),15),2);  movmean(downsample(w(10000+1:end),30),2)];
-semilogx(w,db)
+
+db_mm = [db_m(1:100); movmean(downsample(db_m(100+1:1000),2),3); movmean(downsample(db_m(1000+1:10000),15),2); movmean(downsample(db_m(10000+1:end),30),2)];
+db_um = [db_u(1:100); movmean(downsample(db_u(100+1:1000),2),3); movmean(downsample(db_u(1000+1:10000),15),2); movmean(downsample(db_u(10000+1:end),30),2)];
+w =  [w(1:100);  downsample(w(100+1:1000),2); downsample(w(1000+1:10000),15);  downsample(w(10000+1:end),30)];
+semilogx(w,db_uref)
 
 hold on
+semilogx(w,db_mm)
+%semilogx(w,)
 grid on
-axis([ 0 20000 -40 80])
+axis([ 2 20000 -60 60])
 xlabel('Frequency [Hz]')
 ylabel('SPL [dB]')
+yticks(-50:10:80);
+legend('Without wind','With wind and windscreen, conf 2')
 
+%%
+
+down1 = 2;
+down2 = 15;
+down3 = 70;
+down4 = 160;
+
+load('ingen_vind.mat')
+Fs = 44100;
+L = length(data);
+
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf1u = P1;
+
+X = data(:,2)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf1m = P1;
+
+
+load('ingen_vind_2.mat')
+
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf2u = P1;
+
+X = data(:,2)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf2m = P1;
+
+load('ingen_vind_3.mat')
+
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf3u = P1;
+
+X = data(:,2)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf3m = P1;
+
+%out_ref = (tf1m+tf2m+tf3m)/3;
+%db_ref = 20*log10(abs(out_ref/(20*10^-6)));
+%db_mref = [db_ref(1:100); movmean(downsample(db_ref(100+1:1000),2),3); movmean(downsample(db_ref(1000+1:10000),15),2); movmean(downsample(db_ref(10000+1:end),30),2)];
+
+out_ref = (tf1u+tf2u+tf3u)/3;
+db_ref = 20*log10(abs(out_ref/(20*10^-6)));
+db_uref = [downsample(db_ref(1:716),down1); downsample(db_ref(716+1:7153),down2); downsample(db_ref(7153+1:71536),down3); downsample(db_ref(71536+1:end),down4)];
+
+
+
+load('ingen_vind.mat')
+
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf1u = P1;
+
+X = data(:,2)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf1m = P1;
+
+load('ingen_vind_2.mat')
+
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf2u = P1;
+
+X = data(:,2)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf2m = P1;
+
+load('ingen_vind_3.mat')
+
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf3u = P1;
+
+X = data(:,2)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+tf3m = P1;
+
+f = (Fs*(0:(L/2))/L)';
+
+out_u = (tf1u+tf2u+tf3u)/3;
+out_m = (tf1m+tf2m+tf3m)/3;
+
+db_u = 20*log10(abs(out_u/(20*10^-6)));
+db_m = 20*log10(abs(out_m/(20*10^-6)));
+%db = db_u-db_m;
+%db = movmean(db,5);
+
+db_mm = [downsample(db_m(1:716),down1); downsample(db_m(716+1:7153),down2); downsample(db_m(7153+1:71536),down3); downsample(db_m(71536+1:end),down4)];
+db_um = [downsample(db_u(1:716),down1); downsample(db_u(716+1:7153),down2); downsample(db_u(7153+1:71536),down3); downsample(db_u(71536+1:end),down4)];
+
+f =  [downsample(f(1:716),down1);  downsample(f(716+1:7153),down2); downsample(f(7153+1:71536),down3);  downsample(f(71536+1:end),down4)];
+semilogx(f,db_um)
+
+hold on
+semilogx(f,db_mm)
+%semilogx(w,)
+grid on
+axis([ 2 20000 -40 90])
+xlabel('Frequency [Hz]')
+ylabel('SPL [dB]')
+yticks(-50:10:90);
+legend('Without windscreen','With windscreen, conf 1')
+
+
+%%
+clear
+load('ingen_vind.mat')
+fs = 44100;
+[tf1u,w] = freqz((data(:,1)/0.131931494139380)/length(data),1,20000,fs);
+
+Fs = 44100;
+L = length(data);
+X = data(:,1)/0.131931494139380;
+Y = fft(X);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L/2))/L;
+semilogx(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+hold on 
+semilogx(w,abs(tf1u))
 %%
 load('ingen_vind.mat')
 
